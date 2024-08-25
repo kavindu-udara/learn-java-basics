2967fbd7360674672917fff40b5ee2f4b5bcd95b
kavindu-udara <karunasinghesampath38@gmail.com>
2024-08-25 18:08:09 +0530

    2 D Graphics

--- src/twoDGraphics/MyFrame.java
+++ src/twoDGraphics/MyFrame.java
@@ -1,12 +1,23 @@
 package twoDGraphics;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
+    MyPanel panel;
     MyFrame(){
+        panel = new MyPanel();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
+
+        this.add(panel);
+        this.pack();
+
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
+
+    
 }
